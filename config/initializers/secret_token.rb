e3b55e9caa81903b0f3021d8883c497facbf8259
Tom Henderson <tom@mathpunk.net>
2011-06-09 10:30:43 -0700

    So-called 'initial' fucking commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pmonline::Application.config.secret_token = 'd2cead7e1916fbd59e5aa5e0c5cec7c09e2a5d3a83336e81903a1fbe21cbd3f17d79ce25dd837d6b0d03d7093075d1b8e9071db4db8552039590c45e1d7e197d'
