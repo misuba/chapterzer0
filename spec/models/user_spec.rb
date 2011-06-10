7e5fbddb81a4ec7edb2d6dc62feff33d75675bcc
Tom Henderson <tom@mathpunk.net>
2011-06-09 22:38:01 -0700

    Salted

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,5 +79,13 @@ describe User do
 		it "should set the encrypted password" do
 			@user.encrypted_password.should_not be_blank
 		end
+		describe "has password? method" do
+			it "should be true if the passwords match" do
+				@user.has_password?(@attr[:password]).should be_true
+			end
+			it "should be false if the passwords don't match" do
+				@user.has_password?("invalid").should be_false
+			end
+		end
 	end
 end
