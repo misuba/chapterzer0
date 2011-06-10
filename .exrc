4022a114f08a0c5110b0fd214a8c7d6a072f8d43
Tom Henderson <tom@mathpunk.net>
2011-06-09 20:37:38 -0700

    Length validations for name.

--- .exrc
+++ .exrc
@@ -2,3 +2,4 @@ abbr rs response.should
 abbr hs have_selector
 abbr spech require 'spec_helper'
 abbr sn should_not
+abbr bv be_valid
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,9 @@ describe User do
 		no_name_user = User.new(@attr.merge(:name =>  ""))
 		no_name_user.should_not be_valid
 	end
+	it "should reject names that are too long" do
+		long_name = "a"*51
+		long_name_user = User.new(@attr.merge( :name => long_name ))
+		long_name_user.should_not be_valid 
+	end
 end
