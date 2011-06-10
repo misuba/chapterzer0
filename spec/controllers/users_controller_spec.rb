0c220dbadbcb56cf7b05188d5229e8853e78367c
Tom Henderson <tom@mathpunk.net>
2011-06-10 13:28:07 -0700

    Finished signup form.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -39,7 +39,6 @@ describe UsersController do
 		end
 	end
 	describe "POST 'create'" do
-
 		describe "failure" do
 			before(:each) do
 				@attr = { :name => "", :email => "", :password => "",
@@ -57,6 +56,23 @@ describe UsersController do
 			it "should render the 'new' page" do
 					post :create, :user => @attr
 					response.should render_template('new')
+			end
+		end
+		describe "success" do
+			before(:each) do
+				@attr = { :name => "New User", :email => "user@example.com", 
+					:password => "foobar", :password_confirmation => "foobar" } 
+			end
+
+			it "should create a user" do
+				lambda do
+					post :create, :user => @attr
+				end.should change(User, :count).by(1)
+			end
+
+			it "should redirect to the user page" do
+					post :create, :user => @attr
+					response.should redirect_to(user_path(assigns(:user)))
 			end
 		end
 	end
