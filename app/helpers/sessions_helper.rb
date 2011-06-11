be3248679d825b517d32a989624cb05b7336f701
Tom Henderson <tom@mathpunk.net>
2011-06-11 15:02:21 -0700

    Authentication for edit/update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,4 +24,7 @@ module SessionsHelper
 	def remember_token
 		cookies.signed[:remember_token] || [nil, nil]
 	end
+	def deny_access
+		redirect_to signin_path, :notice =>  "Please sign in to access this page."
+	end
 end
