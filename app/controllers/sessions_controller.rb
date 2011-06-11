7b663fba80368b34915fd8bba73b35ad3c8e6f64
Tom Henderson <tom@mathpunk.net>
2011-06-10 17:54:25 -0700

    Sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
 		end
 	end
 	def destroy
+		sign_out
+		redirect_to root_path
 	end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,38 @@
-require 'rubygems'
 require 'spork'
 
 Spork.prefork do
-	ENV["RAILS_ENV"] ||= 'test'
-	unless defined?(Rails)
-		require File.dirname(__FILE__) + "/../config/environment"
-	end
-	require 'rspec/rails'
+  # Loading more in this block will cause your tests to run faster. However, 
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-	Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+  # Requires supporting files with custom matchers and macros, etc,
+  # in ./support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+  
+  RSpec.configure do |config|
+    # == Mock Framework
+    #
+    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+    #
+    # config.mock_with :mocha
+    # config.mock_with :flexmock
+    # config.mock_with :rr
+    config.mock_with :rspec
 
-	RSpec.configure do |config|
-		config.mock_with :rspec
-		config.fixture_path = "#{::Rails.root}/spec/fixtures"
-		config.use_transactional_fixtures = true
-		ActiveSupport::Dependencies.clear
-	end
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, comment the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = true
+
+    def test_sign_in(user)
+      controller.sign_in(user)
+    end
+  end
 end
 
 Spork.each_run do
