8daac62fbcc744f484f4362682f1963cd05c9ce6
Tom Henderson <tom@mathpunk.net>
2011-08-06 06:29:31 -0700

    Began to stub drones and realized that's a new branch.

---
+++ app/helpers/TODO_textdrone.rb
@@ -0,0 +1,6 @@
+# I'm not really a thing yet.
+# Someday I should be able to:
+#		Count Tom's words for the day
+#		Post word count to twitter
+#		Shame him mercilessly
+#			possibly in verse
---
+++ app/models/drones/vizdrone.rb
@@ -0,0 +1,7 @@
+# Dynamic Pages
+Data = "http://punkmathematics.com/data" 
+	# Controller for data visualizations
+	# First control: textdrone, v1
+	#		textdrone should count the words in my working directory
+	#		textdrone should tweet the word count
+	#		textdrone might rudely comment on how much I've fallen short
