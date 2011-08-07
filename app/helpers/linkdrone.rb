b4406616fc1c7868bf1c2682c690e7cbc5db725e
Tom Henderson <tom@mathpunk.net>
2011-08-06 18:11:05 -0700

    Moved early prototype of linkdrone in

---
+++ app/helpers/linkdrone.rb
@@ -0,0 +1,18 @@
+require "ridiculous" include "Ridiculous"
+
+Ridiculous::USERNAME = "YourDeliciousUsername"  
+Ridiculous::PASSWORD = "YourDeliciousPassword" ...
+
+=begin Post methods
+  add
+  all
+  delete
+  get
+  recent
+
+=begin Tag methods
+  rename
+  tags
+=end
+
+
